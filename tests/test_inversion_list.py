4d18f4891d78375ab9a8d8d5a94bd7970bf79853
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-13 23:22:27 +0200

    Update and rename test_arithmetic.py to test_inversion_list.py

---
+++ tests/test_inversion_list.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from finite_set import *
+
+
+class InversionTestCase(TestCase):
+    def test_create(self):
+        set = IntegerSet([ (1,3), (5,7)]):
+        repr(s)
+        printf(len(s))
+
+    
