e5559533931feaff071952e408793494c02196c4
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-14 17:02:24 +0200

    Update _inversion_list.pyx

--- finite_set/_inversion_list.pyx
+++ finite_set/_inversion_list.pyx
@@ -22,7 +22,7 @@ _init()
 
 
 
-cdef fi.InversionList *_set
+
 class IntegerSet(AbstractSet[int]):
     
     def __init__(
@@ -46,7 +46,7 @@ class IntegerSet(AbstractSet[int]):
             return cls()
         
     def __repr__(self) -> str:
-        return fi.inversion_list_to_string(fi.InversionList(self._set)).decode("UTF-8")
+        return fi.inversion_list_to_string(IntegerSet(fi.InversionList(self._set)).decode("UTF-8")
                                   
     def __hash__(self) -> int: 
         return hash(tuple(self._set.intervals()))
