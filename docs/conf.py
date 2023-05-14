23e212f4d58da90ec31d817f48c75a5509a56a13
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-14 14:15:26 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,8 +5,8 @@ import sys
 
 from datetime import date
 
-author = "Christophe Demko"
-project = "py-arithmetic"
+author = "Groupe H"
+project = "finite_set"
 version = "0.1.0"
 
 year = date.today().year
