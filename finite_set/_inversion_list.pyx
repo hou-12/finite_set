8f23f0a905c3ad40b6e80785f05c6c8c4743f556
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-14 15:22:36 +0200

    Update _inversion_list.pyx

--- finite_set/_inversion_list.pyx
+++ finite_set/_inversion_list.pyx
@@ -3,6 +3,7 @@
 # distutils: extra_compile_args = -O3
 #from collections.abc import AbstractSet
 from typing import Optional, Iterable, Tuple, Iterator
+import collection.abc
 
 
 import finite_set.c_inversion_list as fi
@@ -21,7 +22,7 @@ _init()
 
 
 
-class IntegerSet(AbstractSet[int]):
+class IntegerSet(abc.AbstractSet[int]):
     def __init__(
     self,
     intervals: Optional[Iterable[Tuple[int, int]]] = None,
