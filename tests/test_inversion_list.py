8e32f6f72ea9907729d3f297ef004fb908e5366d
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-17 21:35:15 +0200

    Update test_inversion_list.py

--- tests/test_inversion_list.py
+++ tests/test_inversion_list.py
@@ -3,7 +3,7 @@ import finite_set as f
 
 class InversionTestCase(TestCase):
     def test_create(self):
-        set = f([ (1,3), (5,7)])
+        set = f.__init__([ (1,3), (5,7)])
         repr(set)
         print(len(set))
 
