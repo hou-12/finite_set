e3e1006b52579d710e4086caec06921fea3dc54e
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-17 22:16:05 +0200

    Update test_inversion_list.py

--- tests/test_inversion_list.py
+++ tests/test_inversion_list.py
@@ -1,10 +1,19 @@
 from unittest import TestCase
 from _inversion_list import IntegerSet
-
+"""
 class InversionTestCase(TestCase):
     def test_create(self):
         _set = IntegerSet( [(1, 3), (5, 7)] )
         repr(_set)
-        
+ """
+
+from finite_set import IntegerSet
+
+# Créer un ensemble d'entiers
+s = IntegerSet([(-10, -5), (0, 5), (10, 15)])
+
+# Vérifier si un élément est dans l'ensemble
+print(0 in s) # True
+print(7 in s) # False
 
     
