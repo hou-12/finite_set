434eff8ea1aaf3dd439eb5019ab81d56c6e0dc03
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-17 23:23:33 +0200

    Update _inversion_list.pyx

--- finite_set/_inversion_list.pyx
+++ finite_set/_inversion_list.pyx
@@ -18,6 +18,7 @@ cdef _init():
 
 _init()
 cdef fi.InversionList *_set
+cdef unsigned int a[6] =  {1, 2, 3, 5, 7, 9}
 class IntegerSet(AbstractSet[int]):
     
     
@@ -25,7 +26,7 @@ class IntegerSet(AbstractSet[int]):
     self,
     intervals: Optional[Iterable[Tuple[int, int]]] = None,
     ) -> None: 
-        cdef unsigned int a[6] =  {1, 2, 3, 5, 7, 9}
+        
         if intervals is not None:
             self._set = fi.inversion_list_create(20, sizeof a / sizeof *a, a)
         else:
