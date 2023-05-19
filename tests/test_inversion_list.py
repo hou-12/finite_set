4578797568d1c74cf4c03b62a63338f833a82439
hou-12 <82287928+hou-12@users.noreply.github.com>
2023-05-19 22:41:13 +0200

    Update test_inversion_list.py

--- tests/test_inversion_list.py
+++ tests/test_inversion_list.py
@@ -1,21 +1,34 @@
 from unittest import TestCase
-
+from finite_set import IntegerSet
 
 class InversionTestCase(TestCase):
     def test_create(self):
-        _set = IntegerSet( [(1, 3), (5, 7)] )
-        self.assertEqual(repr(_set), "NULL")
+        
+        g = [(1, 5), (10, 15), (20, 25)]
+        gg = [1, 2, 3, 4, 10, 11, 12, 13, 14, 20, 21, 22, 23, 24]
+        ggg = []
+        _set = IntegerSet( g )
+        self.assertEqual(repr(_set), "[1, 2, 3, 4, 10, 11, 12, 13, 14, 20, 21, 22, 23, 24]")
+        self.assertEqual(len(_set), 14)
+        self.assertEqual(14 in _set, True)
+      
+        for i in _set:
+            ggg.append(i)
 
+        gg = [(1, 5)]
+        ggg =  [(1, 5), (10, 15), (20, 25)]
+        gggg = [(1, 5), (10, 15), (20, 30)]
+        _set = IntegerSet( gg )
+        _set1 = IntegerSet( ggg )
+        _set3 = IntegerSet( gggg )
+
+        self.assertEqual(_set == _set, True)
+
+        self.assertEqual(_set <= _set, True)
+        self.assertEqual(_set >= _set, True)
+            
+        self.assertEqual(_set < _set1, True)
+        self.assertEqual(_set3 > _set1, True)
+        
         _set = IntegerSet()
-        self.assertEqual(repr(_set), "NULL")
- 
-
-from finite_set import IntegerSet
-
-# Créer un ensemble d'entiers
-intervals = [(1,2),(3,5),(6,9)]
-integer_set = IntegerSet(intervals)
-
-
-
-    
+        self.assertEqual(repr(_set), "(null)")
